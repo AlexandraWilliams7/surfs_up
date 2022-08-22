34f980b829d0a0dabef1bc0972aa355a9c93b7a7
AlexandraWilliams7 <a.lain@Alexandra-Williams.com>
2022-08-21 23:42:09 -0500

    adding app.py and updating climate_analysis.

---
+++ app.py
@@ -0,0 +1,10 @@
+# importing dependencies
+from flask import Flask
+
+#create a new flask app instance
+app = Flask(__name__)
+
+# create flask routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
